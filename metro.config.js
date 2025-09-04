68c097cf557cb70195c88f3ad772214ca02d3627
y2kvhmed <y2k.vhmed@gmail.com>
2025-09-04 13:38:41 +0300

    Add files via upload

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,16 @@ const config = getDefaultConfig(__dirname);
 // Add web support
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 
+// Fix import.meta issue for web
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+};
+
+// Web-specific resolver configuration
+config.resolver.alias = {
+  ...config.resolver.alias,
+  'react-native$': 'react-native-web',
+};
+
 module.exports = config;
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  
+  // Fix import.meta issue
+  config.module.rules.push({
+    test: /\.m?js$/,
+    resolve: {
+      fullySpecified: false,
+    },
+  });
+
+  return config;
+};
